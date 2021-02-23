c5a6d74122856ed574d10b9446c519bfb5860508
technicolour24k <35193590+technicolour24k@users.noreply.github.com>
2021-02-23 21:43:04 +0000

    Re-added testing with folder structure

---
+++ testing/README.md
@@ -0,0 +1,2 @@
+# FFXI-Windower-Addons
+Place where I toss random FFXI addon ideas - most are probably a WIP, don't get too excited.
---
+++ testing/testing.lua
@@ -0,0 +1,76 @@
+--Copyright (c) 2019, Kin
+--All rights reserved.
+
+--Redistribution and use in source and binary forms, with or without
+--modification, are permitted provided that the following conditions are met:
+
+--    * Redistributions of source code must retain the above copyright
+--      notice, this list of conditions and the following disclaimer.
+--    * Redistributions in binary form must reproduce the above copyright
+--      notice, this list of conditions and the following disclaimer in the
+--      documentation and/or other materials provided with the distribution.
+--    * Neither the name of Auto Enter Key nor the
+--      names of its contributors may be used to endorse or promote products
+--      derived from this software without specific prior written permission.
+
+--THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+--ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+--WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+--DISCLAIMED. IN NO EVENT SHALL Chiaia BE LIABLE FOR ANY
+--DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+--(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+--LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+--ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+--(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+--SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+_addon.name = 'Testing'
+_addon.version = '1.0'
+_addon.author = 'Kin [Nocturnal Souls]'
+_addon.commands = {'test',}
+
+
+items = {4545,1179, 4538}
+
+
+
+function main()
+	bag = 0
+    print("Loaded...")
+    local invTable = windower.ffxi.get_bag_info(bag)
+    local invItemCount = windower.ffxi.get_items(bag)
+    -- tossList = {}
+    -- local file = io.open(windower.addon_path.. "tossList.txt")
+    -- local i = 0
+    -- if file then
+    --     for line in file:lines() do
+    --     i = i + 1
+    --     tossList[i] = line
+    --     end
+    --     file:close()
+    -- else
+    --     error('file not found')
+    -- end
+
+    n = 0
+    while (n < invTable.max) do
+        local itemInfo = windower.ffxi.get_items(0, n)
+        local itemCount = itemInfo.count
+        count = 1
+        while (count <= table.getn(items)) do
+
+            -- print("Count: " ..count)
+            -- print("items[count]: " ..items[count])
+            if (itemInfo.id == items[count]) then
+                print("Found " ..itemInfo.id.." at " ..itemInfo.slot)
+                windower.ffxi.drop_item(itemInfo.slot, 1)
+                -- break
+            end
+            count = count+1
+        end
+        n = n+1
+    end
+end
+
+windower.register_event('load', main)
+windower.register_event('add item', 0, main)
